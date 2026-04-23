8ba7d499c208700730517fca26c2fb19a233dd6f
Vick <victoriadias99@outlook.com>
2026-04-23 17:15:02 -0300

    fix: enable PHP CLI server workers (10) to process concurrent requests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec php -S 0.0.0.0:${PORT:-8080} -t public_html public_html/router.php 2>&1
+exec env PHP_CLI_SERVER_WORKERS=10 php -S 0.0.0.0:${PORT:-8080} -t public_html public_html/router.php 2>&1
